3288217067af5956c68330874d9fe5b0baea7bbd
Julien Fleury <julienfleury93@gmail.com>
2022-10-31 14:14:52 +0100

    docs: updated eslint

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const x = 'sfsdfsd';
+  const x = "sfsdf";
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -15,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
