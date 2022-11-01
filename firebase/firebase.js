e146deecd591ccef4d59f5cb3f3e9c86a5632846
Julien Fleury <julienfleury93@gmail.com>
2022-11-01 20:14:19 +0100

    feat: firebase added temporarily. Email and PW inputs complete

---
+++ firebase/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+import { KEYS } from "../keys";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: KEYS.FIREBASE_API,
+    authDomain: "task-list-application.firebaseapp.com",
+    projectId: "task-list-application",
+    storageBucket: "task-list-application.appspot.com",
+    messagingSenderId: "906645578462",
+    appId: "1:906645578462:web:a2f710ca8340e99f6e584f",
+    measurementId: "G-QY3YXG8XYE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
--- screens/login/login.screen.jsx
+++ screens/login/login.screen.jsx
@@ -8,18 +8,22 @@ import { colors } from '../../theme/colors';
 
 const LoginContainer = styled(View)`
   flex: 1;
-  justify-content: center;
   padding: ${(props) => props.theme.size[3]};
 `;
 
-const LoginEmailInput = styled(TextInput)``;
+const LoginEmailInput = styled(TextInput)`
+  margin-top: ${(props) => props.theme.size[6]};
+  margin-bottom: ${(props) => props.theme.size[2]};
+`;
+const LoginPasswordInput = styled(TextInput)``;
 
 // eslint-disable-next-line react/prop-types
 export const LoginScreen = ({ setIsLoggedIn }) => {
   const { userUid, setUserUid } = useContext(UserContext);
   const [loginEmail, setLoginEmail] = useState('');
   const [emailInvalid, setEmailInvalid] = useState(false);
-  const [loingPassword, setLoginPassword] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const onLoginEmailChange = (email) => {
     if (
@@ -47,6 +51,28 @@ export const LoginScreen = ({ setIsLoggedIn }) => {
           outlineColor={colors.primaryBlue}
           activeOutlineColor={colors.primaryBlue}
           error={emailInvalid}
+        />
+        <LoginPasswordInput
+          label={'Password'}
+          mode={'outlined'}
+          right={
+            showPassword ? (
+              <TextInput.Icon
+                name={'eye'}
+                onPress={() => setShowPassword(false)}
+              />
+            ) : (
+              <TextInput.Icon
+                name={'eye-off'}
+                onPress={() => setShowPassword(true)}
+              />
+            )
+          }
+          secureTextEntry={!showPassword}
+          value={loginPassword}
+          onChangeText={(text) => setLoginPassword(text)}
+          outlineColor={colors.primaryBlue}
+          activeOutlineColor={colors.primaryBlue}
         />
       </LoginContainer>
     </SafeArea>
